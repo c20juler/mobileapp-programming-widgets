1e249e8031af0a344b5f2aea73ff9daf5879790d
c20juler <c20juler@student.his.se>
2021-04-14 13:15:36 +0200

    Added a onclick method in OnCreate for my button and a Toast message "Hello!"

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -6,13 +6,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button my_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        my_button = (Button) findViewById(R.id.minKnapp);
+
+        my_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","onClick worked!");
+                Toast.makeText(MainActivity.this, "Hello!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
